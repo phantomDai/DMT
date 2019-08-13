5031fb325501e9c703a80f96eec78d531eab6875
phantom <daihepeng@sina.cn>
2019-08-13 21:13:14 +0800

    buzai shiyong fn

--- src/main/java/labprograms/result/RecordResult.java
+++ src/main/java/labprograms/result/RecordResult.java
@@ -26,10 +26,7 @@ public class RecordResult {
                                     double averageFmeasure, double averageTmeasure,
                                     double averagefirstSelete, double averagefirstGenerate,
                                     double averagefirstExecute, double averageallSelete,
-                                    double averageallGenerate, double averageallExecute,
-                                    int index, List<Integer> f2measure,List<Long> secondSeleteArray,List<Long> secondGenerateArray,
-                                    List<Long> secondExecuteArray, double averageF2measure,double averageSecondSelete,double averageSecondGenerate,
-                                    double averageSecondExecute){
+                                    double averageallGenerate, double averageallExecute){
 
         String path = Constant.resultPath +separator + objectName;
         File file = new File(path);
@@ -42,28 +39,20 @@ public class RecordResult {
         }
         try {
             PrintWriter printWriter = new PrintWriter(new FileWriter(file,true));
-            printWriter.write("index:" + String.valueOf(index) + "\n");
             printWriter.write("F:" + fmeasure.toString() + "\n");
             printWriter.write("T:" + tmeasure.toString() + "\n");
             printWriter.write("firstSeleteArray:" + firstSeleteArray.toString() + "\n");
             printWriter.write("firstGenerateArray:" + firstGenerateArray.toString() + "\n");
             printWriter.write("firstExecuteArray:" + firstExecuteArray.toString() + "\n");
-            printWriter.write("secondSeleteArray:" + secondSeleteArray.toString() + "\n");
-            printWriter.write("secondGenerateArray:" + secondGenerateArray.toString() + "\n");
-            printWriter.write("secondExecuteArray:" + secondExecuteArray.toString() + "\n");
             printWriter.write("allSeleteArray:" + allSeleteArray.toString() + "\n");
             printWriter.write("allGenerateArray:" + allGenerateArray.toString() + "\n");
             printWriter.write("allExecuteArray:" + allExecuteArray.toString() + "\n");
 
             printWriter.write("averageF:" + String.valueOf(averageFmeasure) + "\n");
-            printWriter.write("averageF2:" + String.valueOf(averageF2measure) + "\n");
             printWriter.write("averageT:" + String.valueOf(averageTmeasure) + "\n");
             printWriter.write("averagefirstSelete:" + String.valueOf(averagefirstSelete) + "\n");
             printWriter.write("averagefirstGenerate:" + String.valueOf(averagefirstGenerate) + "\n");
             printWriter.write("averagefirstExecute:" + String.valueOf(averagefirstExecute) + "\n");
-            printWriter.write("averagesecondSelete:" + String.valueOf(averageSecondSelete) + "\n");
-            printWriter.write("averagesecondGenerate:" + String.valueOf(averageSecondGenerate) + "\n");
-            printWriter.write("averagesecondExecute:" + String.valueOf(averageSecondExecute) + "\n");
             printWriter.write("averageallSelete:" + String.valueOf(averageallSelete) + "\n");
             printWriter.write("averageallGenerate:" + String.valueOf(averageallGenerate) + "\n");
             printWriter.write("averageallExecute:" + String.valueOf(averageallExecute) + "\n");
